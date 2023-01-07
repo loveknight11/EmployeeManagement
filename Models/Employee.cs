913f533089c3993b40ae9e8445c629f8e3bb59b4
Alhussiny <eng.elhoseny.osama@gmail.com>
2023-01-07 20:41:56 +0200

    valdate select list

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -9,14 +9,18 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+        
         [Required]
         [MaxLength(50, ErrorMessage ="Can't Exceed 50 characters")]
         public string Name { get; set; }
+        
         [Required]
         [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
             , ErrorMessage ="Invalid EmailFormat")]
         [Display(Name ="Office Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+        
+        [Required]
+        public Dept? Department { get; set; }
     }
 }
