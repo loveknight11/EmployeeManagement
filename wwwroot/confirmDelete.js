8652edc25aad4bdc892c48d769f747c772ec630f
Alhussiny <eng.elhoseny.osama@gmail.com>
2023-02-04 19:59:24 +0200

    confirm delete using custom java function

---
+++ wwwroot/confirmDelete.js
@@ -0,0 +1,11 @@
+﻿function ConfirmDelete(id, isDeleteClicked) {
+    var deleteSpan = 'deleteSpan_' + id;
+    var confirmSpan = 'confirmSpan_' + id;
+    if (isDeleteClicked) {
+        $('#' + confirmSpan).show();
+        $('#' + deleteSpan).hide();
+    } else {
+        $('#' + confirmSpan).hide();
+        $('#' + deleteSpan).show();
+    }
+}
